bb8e1e0277e79fb369640026783f798d87b8ec21
Johnsonkdonald <Johnson.k.donald@gmail.com>
2016-04-10 10:45:35 -0700

    Update truncate.js

--- truncate.js
+++ truncate.js
@@ -3,6 +3,21 @@
 // TODO: Create a Function called truncateWords() that accepts two arguments: 
 // longText (a String with several words in it)
 // numWords (an Integer that sets the number of words you want in the returned text)
+var truncateWords = function(longText, numWords){
+	var text_arr = new Array();
+	// Split the longText by space and store it in array
+	text_arr = longText.split(" ");
+
+	// Splice the array from the starting index (numWords) until the end of array
+	text_arr.splice(numWords,text_arr.length-1);
+
+	// Add an ellipses string to the array
+	text_arr.push("...");
+
+	// Convert back the array to String and return it
+	return text_arr.join(" ");
+};
+
 
 // TODO: Within the truncateWords() Function, complete the following steps:
 // 1. Use the split() function to split the String into an Array
